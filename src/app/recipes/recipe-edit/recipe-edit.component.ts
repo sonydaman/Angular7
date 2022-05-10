68926aa42ebf1c3bc914e6d36ab936265782f631
Gurbaj1000 <gurbaj1000@gmail.com>
2022-05-10 03:52:51 -0700

    Validating user input

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormArray, FormControl, FormGroup } from '@angular/forms';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Params } from '@angular/router';
 import { RecipeService } from '../recipe.service';
 
@@ -35,8 +35,11 @@ recipeForm: FormGroup;
     onAddIngredient(){
       (<FormArray>this.recipeForm.get('ingredients')).push(
         new FormGroup({
-          'name': new FormControl(),
-          'amount': new FormControl()
+          'name': new FormControl(null, Validators.required),
+          'amount': new FormControl(null, [
+            Validators.required,
+            Validators.pattern(/^[1-9]+[0-9]*$/)
+          ])
         })
       )
     }
@@ -54,8 +57,11 @@ if (recipe['ingredients']) {
   for (let ingredient of recipe.ingredients){
     recipeIngredients.push(
       new FormGroup({
-        'name': new FormControl(ingredient.name),
-        'amount': new FormControl(ingredient.amount)
+        'name': new FormControl(ingredient.name, Validators.required),
+        'amount': new FormControl(ingredient.amount,[
+           Validators.required,
+            Validators.pattern(/^[1-9]+[0-9]*$/)
+        ])
       })
     );
   }
@@ -63,9 +69,9 @@ if (recipe['ingredients']) {
     }
   
 this.recipeForm = new FormGroup({
-  'name': new FormControl(recipeName),
-  'imagePath': new FormControl(recipeImagePath),
-  'description': new FormControl(recipeDescription),
+  'name': new FormControl(recipeName, Validators.required),
+  'imagePath': new FormControl(recipeImagePath, Validators.required),
+  'description': new FormControl(recipeDescription, Validators.required),
   'ingredients': recipeIngredients,
 })
   
