ebadb875d785afe5bb084b63698b3c5fda1e5d75
Gurbaj1000 <gurbaj1000@gmail.com>
2022-05-10 02:08:27 -0700

    Sync form with html & editing recipe page

--- src/app/recipes/recipe.model.ts
+++ src/app/recipes/recipe.model.ts
@@ -3,12 +3,12 @@ import { Ingredient } from "../shared/ingredient.model";
 export class Recipe{
     public name: string;
     public description: string;
-    public imagepath: string;
+    public imagePath: string;
     public ingredients: Ingredient[];
-    constructor(name:  string, desc: string, imagepath: string, ingredients: Ingredient[]){
+    constructor(name:  string, desc: string, imagePath: string, ingredients: Ingredient[]){
         this.name = name;
         this.description = desc;
-        this.imagepath = imagepath;
+        this.imagePath = imagePath;
     }
 }
 
