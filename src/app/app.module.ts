0a174fbe7234354b840f3d7f83a79122792caab5
Gurbaj1000 <gurbaj1000@gmail.com>
2022-05-13 03:38:01 -0700

    Http and Firebase set

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
@@ -39,6 +40,7 @@ import { RecipeService } from './recipes/recipe.service';
     AppRoutingModule,
     ReactiveFormsModule,
     FormsModule,
+    HttpClientModule,
     ReactiveFormsModule,
     
     ServiceWorkerModule.register('ngsw-worker.js', {
---
+++ src/app/shared/data-storage.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { RecipeService } from "../recipes/recipe.service";
+@Injectable({providedIn: 'root'})
+export class DataStorageService {
+    constructor(private http: HttpClient, private recipService: RecipeService) {}
+
+    storeRecipes() {
+        const recipes = this.recipService.getRecipes();
+        this.http.put('https://my-course-recipe-d06f8-default-rtdb.firebaseio.com/recipes.json', recipes).subscribe(Response => {
+            console.log(Response);
+        });
+    }
+}
