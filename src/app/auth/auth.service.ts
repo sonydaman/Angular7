613ef3719be65b8c4c7afc26b9415237c06bf4ec
Gurbaj1000 <gurbaj1000@gmail.com>
2022-05-18 02:55:57 -0700

    Improved error handling

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { throwError } from "rxjs";
+import { catchError } from "rxjs/operators";
 
 interface AuthResponseData {
     idToken: string;
@@ -20,7 +22,17 @@ export class AuthService {
             password: password,
             returnSecureToken: true
         }
-        );
+        ).pipe(catchError(errorRes => {
+            let errorMessage = 'An unknown error occurred!';
+            if (!errorRes.error || !errorRes.error.error){
+                return throwError(errorMessage);
+            }
+            switch (errorRes.error.error.message){
+                case 'EMAIL_EXISTS':
+                errorMessage = 'This email exists already';
+            }
+            return throwError(errorMessage);
+        }));
         
     }
 }
