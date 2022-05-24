8f83ade8270a1fe9d9936d22af72cb5a234681cb
Gurbaj1000 <gurbaj1000@gmail.com>
2022-05-24 04:03:34 -0700

    Routes to feature Modules(manage not Working)

--- src/app/recipes/recipes.module.ts
+++ src/app/recipes/recipes.module.ts
@@ -8,6 +8,7 @@ import { RecipeStartComponent } from "./recipe-start/recipe-start.component";
 import { RouterModule } from "@angular/router";
 import { CommonModule } from "@angular/common";
 import { ReactiveFormsModule } from "@angular/forms";
+import { RecipeRoutingModule } from "./recipes-routing.module";
 @NgModule({
     declarations: [
         RecipesComponent,
@@ -17,7 +18,7 @@ import { ReactiveFormsModule } from "@angular/forms";
         RecipeEditComponent,
         RecipeStartComponent,
     ],
-    imports: [RouterModule, CommonModule, ReactiveFormsModule],
+    imports: [RouterModule, CommonModule, ReactiveFormsModule, RecipeRoutingModule],
     exports: [
         RecipesComponent,
         RecipeListComponent,
