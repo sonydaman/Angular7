b546dfe41462744a6db20ab66fc3d2a9277627d7
Gurbaj1000 <gurbaj1000@gmail.com>
2022-05-24 03:48:21 -0700

    Splitting Modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,39 +6,28 @@ import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 import { HeaderComponent } from './header/header.component';
-import { RecipesComponent } from './recipes/recipes.component';
-import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
-import { RecipeDetailsComponent } from './recipes/recipe-details/recipe-details.component';
-import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { DropdownDirective } from './shared/dropdown.directive';
 import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
-import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 import { RecipeService } from './recipes/recipe.service';
 import { AuthComponent } from './auth/auth.component';
 import { LoadingSpinnerComponent } from './shared/loading-spinner/loading-spinner.component';
 import { AuthInterceptorService } from './auth/auth-interceptor.service';
 import { AlertComponent } from './shared/alert/alert.component';
 import { PLaceHolderDirective } from './shared/placeholder/placeholder.directive';
+import { RecipesModule } from './recipes/recipes.module';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    RecipesComponent,
-    RecipeListComponent,
-    RecipeDetailsComponent,
-    RecipeItemComponent,
     ShoppingListComponent,
     ShoppingEditComponent,
     DropdownDirective,
-    RecipeStartComponent,
     AuthComponent,
-    RecipeEditComponent,
     LoadingSpinnerComponent,
     AlertComponent,
     PLaceHolderDirective
@@ -52,6 +41,7 @@ import { PLaceHolderDirective } from './shared/placeholder/placeholder.directive
     FormsModule,
     HttpClientModule,
     ReactiveFormsModule,
+    RecipesModule,
     
     ServiceWorkerModule.register('ngsw-worker.js', {
       enabled: environment.production,
---
+++ src/app/recipes/recipes.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from "@angular/core";
+import { RecipeDetailsComponent } from "./recipe-details/recipe-details.component";
+import { RecipeEditComponent } from "./recipe-edit/recipe-edit.component";
+import { RecipeItemComponent } from "./recipe-list/recipe-item/recipe-item.component";
+import { RecipeListComponent } from "./recipe-list/recipe-list.component";
+import { RecipesComponent } from "./recipes.component";
+import { RecipeStartComponent } from "./recipe-start/recipe-start.component";
+import { RouterModule } from "@angular/router";
+import { CommonModule } from "@angular/common";
+import { ReactiveFormsModule } from "@angular/forms";
+@NgModule({
+    declarations: [
+        RecipesComponent,
+        RecipeListComponent,
+        RecipeDetailsComponent,
+        RecipeItemComponent,
+        RecipeEditComponent,
+        RecipeStartComponent,
+    ],
+    imports: [RouterModule, CommonModule, ReactiveFormsModule],
+    exports: [
+        RecipesComponent,
+        RecipeListComponent,
+        RecipeDetailsComponent,
+        RecipeItemComponent,
+        RecipeEditComponent,
+        RecipeStartComponent,  
+    ]
+})
+export class RecipesModule {}
