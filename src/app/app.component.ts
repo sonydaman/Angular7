fd794606624cd34a99bf07e6609788a60bcc2184
Gurbaj1000 <gurbaj1000@gmail.com>
2022-06-09 02:39:24 -0700

    Angular Animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject , PLATFORM_ID} from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 import { AuthService } from './auth/auth.service';
 import { LoggingService } from './logging.service';
 @Component({
@@ -7,8 +8,19 @@ import { LoggingService } from './logging.service';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
- constructor(private authService: AuthService, private loggingService: LoggingService){}
+  store: any;
+  authService: any;
+
+ constructor(
+  // private store: Store<fromApp.AppState>,
+  authService: AuthService, private loggingService: LoggingService,
+  // @Inject(PLATFORM_ID) private platformId
+  ){}
+
   ngOnInit() {
+    // if (isPlatformBrowser(this.platformId)) {
+    //   // this.store.dispatch(new AuthActions.AutoLogin());
+    // }
     this.authService.autoLogin();
     this.loggingService.printLog('Hello from AppComponent ngOnInit');
   }
